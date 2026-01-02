31cda9ca4f0b2db1a6b1c0c49b27be500ff2bad1
james-cavanaugh <james.arr.cavanaugh@gmail.com>
2026-01-01 22:59:09 -0500

    first commit

--- gui.py
+++ gui.py
@@ -8,7 +8,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.core.window import Window
 
 class Fastr(BoxLayout):
     def __init__(self, **kwargs):
